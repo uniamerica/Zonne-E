ff8e194e7305884cb73c3ece86e0a0d3ff39a14e
Vinicius Posser <viniciusposser37@gmail.com>
2021-08-23 19:38:51 -0300

    organizando pastas

--- backend/src/main/java/br/com/zonne/api/models/UserModel.java
+++ backend/src/main/java/br/com/zonne/api/models/UserModel.java
@@ -1,4 +1,28 @@
 package br.com.zonne.api.models;
 
+import lombok.*;
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class UserModel {
+
+    @Id
+    private String cpf;
+
+    private String userName;
+    private String userLastName;
+    private String userEmail;
+    private String userPassword;
+
+    public UserModel(UserModel entity){
+        cpf = entity.getCpf();
+        userName = entity.getUserName();
+        userLastName = entity.getUserLastName();
+        userEmail = entity.getUserEmail();
+        userPassword = entity.getUserPassword();
+    }
 }
