7f09a76097f614fc3b91271f02578f839e1152b3
Johann <johannteixeira2@gmail.com>
2021-09-13 19:05:07 -0300

    Alterando repository

--- backend/src/main/java/br/com/zonne/api/repositories/DealershipRepository.java
+++ backend/src/main/java/br/com/zonne/api/repositories/DealershipRepository.java
@@ -1,16 +1,9 @@
 package br.com.zonne.api.repositories;
 
-<<<<<<< HEAD
-import br.com.zonne.api.models.DeviceModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface DealershipRepository extends JpaRepository<DeviceModel, String>{
-
-=======
 import br.com.zonne.api.models.DealershipModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+
 public interface DealershipRepository  extends  JpaRepository<DealershipModel, Long>{
->>>>>>> 59ece6ec2dc4bd22ef53569e04cfac1642ce414d
+    
 }
