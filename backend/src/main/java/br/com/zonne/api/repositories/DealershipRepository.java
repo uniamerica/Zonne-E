e7111b5ba4bb9d928f7b756c43f9572cff3631d1
Johann <johannteixeira2@gmail.com>
2021-09-14 20:32:52 -0300

    Dealership atualizacao

--- backend/src/main/java/br/com/zonne/api/repositories/DealershipRepository.java
+++ backend/src/main/java/br/com/zonne/api/repositories/DealershipRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface DealershipRepository  extends  JpaRepository<DealershipModel, Long>{
-    
+
 }
---
+++ backend/src/test/java/controllers/DealershipControllerTest.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import br.com.zonne.api.controllers.DealershipController;
+
+public class DealershipControllerTest {
+
+    private DealershipController dealershipController;
+
+    public void (){
+
+    }
+}
