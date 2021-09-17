11e7872df539354d90c9e0eae7c54fcc9d720103
Johann <johannteixeira2@gmail.com>
2021-09-16 21:28:33 -0300

    arrumando bug

--- backend/src/main/java/br/com/zonne/api/controllers/FederativeUnitController.java
+++ backend/src/main/java/br/com/zonne/api/controllers/FederativeUnitController.java
@@ -13,7 +13,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/district")
+@RequestMapping(value = "/federative-unit")
 public class FederativeUnitController {
 
     @Autowired
