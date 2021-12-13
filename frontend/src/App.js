25e52a887022e731629670b137ca0c3b506f31cf
Johann <johannteixeira2@gmail.com>
2021-12-13 19:31:45 -0300

    login em andamento

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Produto from "./pages/Produto/Produto";
+import Login from "./pages/Login/Login";
 import NotFound from "./pages/NotFound/NotFound";
 import Header from "./components/header/Header";
 import ScrollToTop from "./components/ScrollToTop";
@@ -14,7 +15,8 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/produto" component={Produto} />
-        <Route path="*" component={NotFound} />        
+        <Route path="/login" component={Login} />
+        <Route path="*" component={NotFound} />    
       </Switch>
     </Router>
   );
